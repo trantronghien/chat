1fdd99031c75a1821161f3d1f69dc579bb745235
trantronghien <hellotrun@gmail.com>
2016-05-13 20:14:06 +0700

    13/5

---
+++ app/build/generated/source/buildConfig/debug/org/alljoyn/bus/sample/chat/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.alljoyn.bus.sample.chat;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.alljoyn.bus.sample.chat";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 15090000;
+  public static final String VERSION_NAME = "15.09.00";
+}
